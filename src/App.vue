31b625721f4045026f71f440c2924363a282032d
Luca <lucagcuello@gmail.com>
2023-09-15 12:17:28 -0300

    Added base styles, variants and fonts

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>Hello world</h1>
+  <img src="logo.svg" />
+  <h1>Welcome to BookBuddy!</h1>
+  <p>
+    Easily organize and display your favorite books by name and authors. Explore your literary world
+    at a glance.
+  </p>
 </template>
 
-<style scoped></style>
+<style>
+@import url('../src/assets/base.css');
+body {
+  background-color: var(--background-color);
+}
+h1 {
+  font-family: var(--avenir-font-black);
+}
+p {
+  font-family: var(--minion-font-regular);
+}
+</style>
