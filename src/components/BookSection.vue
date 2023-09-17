faf0ca82a89401d4b8d6fac627ba60a34a2cdcac
Luca <lucagcuello@gmail.com>
2023-09-17 14:32:47 -0300

    Created new child comp por formsection for code readability

---
+++ src/components/BookSection.vue
@@ -0,0 +1,178 @@
+<script lang="ts" setup>
+import { type bookObject } from '../types/Types'
+
+const { bookList, removeBook, markAsRead } = defineProps<{
+  bookList: bookObject[]
+  removeBook: (selectedBook: {}) => void
+  markAsRead: (index: number) => void
+}>()
+</script>
+
+<template>
+  <div class="book-container">
+    <template v-for="(book, index) in bookList" :key="book.name">
+      <div class="book" :class="book.color">
+        <div class="book-title-container">
+          <img src="logo.svg" draggable="false" alt="book logo" />
+          <span class="book-name">{{ book.name }}</span>
+        </div>
+        <template v-if="book.read">
+          <v-icon
+            name="bi-bookmark-check-fill"
+            class="read-btn read-filled"
+            @click="markAsRead(index)"
+          />
+        </template>
+        <template v-else>
+          <v-icon name="bi-bookmark-check" class="read-btn" @click="markAsRead(index)" />
+        </template>
+        <v-icon name="bi-trash" class="delete-btn" @click="removeBook(book)" />
+        <p class="book-description">{{ book.description }}</p>
+        <div class="book-author-container">
+          <div class="line"></div>
+          <span class="book-author">{{ book.author }}</span>
+        </div>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'BookSection'
+}
+</script>
+
+<style scoped>
+.book-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-self: center;
+  gap: 30px;
+  width: 100%;
+  max-width: 1000px;
+}
+.book {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+  position: relative;
+  overflow: hidden;
+  width: 50px;
+  height: 420px;
+  gap: 15px;
+  border-radius: 15px 15px 0px 0px;
+  padding: 15px 0;
+  transition: all 200ms ease;
+}
+
+.delete-btn,
+.read-btn {
+  position: absolute;
+  cursor: pointer;
+  opacity: 0;
+  right: 15px;
+  color: var(--main-color);
+  transition: all 200ms ease;
+}
+
+.read-btn {
+  right: unset;
+  left: 15px;
+}
+
+.read-btn:hover {
+  transform: scale(1.2);
+  transition: all 200ms ease;
+}
+
+.delete-btn:hover {
+  transform: scale(1.2);
+  transition: all 200ms ease;
+}
+.book-title-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 15px;
+  width: 100%;
+}
+
+.book-author-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 25px;
+  width: 100%;
+}
+
+.book span {
+  color: #00000094;
+  writing-mode: vertical-rl;
+  text-orientation: mixed;
+  text-align: center;
+}
+
+.book-name {
+  font-family: var(--avenir-font-book);
+  font-weight: 800;
+  font-size: 20px;
+}
+
+.book-author {
+  font-family: var(--minion-font-italic);
+  font-weight: 800;
+}
+
+.book-description {
+  position: absolute;
+  width: 100%;
+  padding: 0px 10px;
+  font-size: 14px;
+  opacity: 0;
+}
+.book img {
+  width: 40px;
+  height: auto;
+}
+.line {
+  width: 100%;
+  height: 1px;
+  background-color: #00000041;
+}
+
+.book:hover {
+  width: 250px;
+  transition: all 200ms ease;
+}
+
+.book:hover .delete-btn,
+.book:hover .read-btn {
+  opacity: 1;
+}
+
+.book:hover span {
+  writing-mode: horizontal-tb;
+  transition: all 600ms ease-in;
+  animation: textOpacity 1s forwards;
+}
+
+.book:hover p {
+  position: static;
+  opacity: 1;
+  transition: all 1s ease-in-out;
+}
+
+@keyframes textOpacity {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
--- src/components/FormSection.vue
+++ src/components/FormSection.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import { type bookObject } from '../types/Types'
+import BookSection from './BookSection.vue'
 
 const bookName = ref(''),
   bookAuthor = ref(''),
@@ -17,7 +18,6 @@ const submitForm = () => {
     read: false
   }
 
-  console.log(item)
   saveToLocalStorage(item)
   clearFormValues()
 }
@@ -114,32 +114,7 @@ onMounted(() => {
     ></textarea>
     <button type="submit">Add to the collection</button>
   </form>
-  <div class="book-container">
-    <template v-for="(book, index) in bookList" :key="book.name">
-      <div class="book" :class="book.color">
-        <div class="book-title-container">
-          <img src="logo.svg" draggable="false" alt="book logo" />
-          <span class="book-name">{{ book.name }}</span>
-        </div>
-        <template v-if="book.read">
-          <v-icon
-            name="bi-bookmark-check-fill"
-            class="read-btn read-filled"
-            @click="markAsRead(index)"
-          />
-        </template>
-        <template v-else>
-          <v-icon name="bi-bookmark-check" class="read-btn" @click="markAsRead(index)" />
-        </template>
-        <v-icon name="bi-trash" class="delete-btn" @click="removeBook(book)" />
-        <p class="book-description">{{ book.description }}</p>
-        <div class="book-author-container">
-          <div class="line"></div>
-          <span class="book-author">{{ book.author }}</span>
-        </div>
-      </div>
-    </template>
-  </div>
+  <BookSection :book-list="bookList" :mark-as-read="markAsRead" :remove-book="removeBook" />
 </template>
 
 <script lang="ts">
@@ -184,164 +159,8 @@ export default {
   width: 100%;
   height: 100%;
 }
-.peppermint,
-.book-color-peppermint {
-  background-color: var(--book-color-peppermint);
-}
-
-.yellow,
-.book-color-yellow {
-  background-color: var(--book-color-yellow);
-}
-
-.sky,
-.book-color-sky {
-  background-color: var(--book-color-sky);
-}
-
-.peach,
-.book-color-peach {
-  background-color: var(--book-color-peach);
-}
-
-.lavender,
-.book-color-lavender {
-  background-color: var(--book-color-lavender);
-}
 
 .bookshelf-color-container input:checked + label {
   box-shadow: 0px 2px var(--main-color);
-}
-
-.book-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  align-self: center;
-  gap: 30px;
-  width: 100%;
-  max-width: 1000px;
-}
-.book {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex-direction: column;
-  position: relative;
-  overflow: hidden;
-  width: 50px;
-  height: 420px;
-  gap: 15px;
-  border-radius: 15px 15px 0px 0px;
-  padding: 15px 0;
-  transition: all 200ms ease;
-}
-
-.delete-btn,
-.read-btn {
-  position: absolute;
-  cursor: pointer;
-  opacity: 0;
-  right: 15px;
-  color: var(--main-color);
-  transition: all 200ms ease;
-}
-
-.read-btn {
-  right: unset;
-  left: 15px;
-}
-
-.read-btn:hover {
-  transform: scale(1.2);
-  transition: all 200ms ease;
-}
-
-.delete-btn:hover {
-  transform: scale(1.2);
-  transition: all 200ms ease;
-}
-.book-title-container {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 15px;
-  width: 100%;
-}
-
-.book-author-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  gap: 25px;
-  width: 100%;
-}
-
-.book span {
-  color: #00000094;
-  writing-mode: vertical-rl;
-  text-orientation: mixed;
-  text-align: center;
-}
-
-.book-name {
-  font-family: var(--avenir-font-book);
-  font-weight: 800;
-  font-size: 20px;
-}
-
-.book-author {
-  font-family: var(--minion-font-italic);
-  font-weight: 800;
-}
-
-.book-description {
-  position: absolute;
-  width: 100%;
-  padding: 0px 10px;
-  font-size: 14px;
-  opacity: 0;
-}
-.book img {
-  width: 40px;
-  height: auto;
-}
-.line {
-  width: 100%;
-  height: 1px;
-  background-color: #00000041;
-}
-
-.book:hover {
-  width: 250px;
-  transition: all 200ms ease;
-}
-
-.book:hover .delete-btn,
-.book:hover .read-btn {
-  opacity: 1;
-}
-
-.book:hover span {
-  writing-mode: horizontal-tb;
-  transition: all 600ms ease-in;
-  animation: textOpacity 1s forwards;
-}
-
-.book:hover p {
-  position: static;
-  opacity: 1;
-  transition: all 1s ease-in-out;
-}
-
-@keyframes textOpacity {
-  0% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 1;
-  }
 }
 </style>
